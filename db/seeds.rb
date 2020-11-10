e7cadd56366ca62bd08ad976120dded2dd5f1537
Thomas Monvoisin <thomasmonvoisin@gmail.com>
2020-11-10 18:20:30 +0100

    Finished Task Manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  task = Task.new(
+    title: Faker::Lorem.word,
+    details: Faker::Quote.yoda,
+  )
+  task.save!
+end
